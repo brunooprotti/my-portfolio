fa3e54eedb29df248f56bee77483dd18e9c77968
Bruno Protti <brunooprotti@gmail.com>
2024-02-06 23:58:14 -0300

    education info added

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { education } from '@cv';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+  public titleWork: string = education.titleSection;
+  public listEducations = education.listEducations;
 
+
+  public takeStartDate(date: string): number {
+    return new Date(date).getUTCFullYear();
+  }
+  public takeEndDate(date: string | undefined): number | string{
+    return date !== undefined  ? ` - ${new Date(date).getUTCFullYear()}` : "";
+  }
 }
