dfdfb54057dd314600843507e235181a3a4ff215
Bruno Protti <bruno.protti@starkit.es>
2025-03-17 14:46:15 -0300

    Add profile image and new experience + years diff

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -11,6 +11,36 @@ export class ExperienceComponent {
   public listExperience = work.experience;
 
 
+  public calculateTime(startDate: string, endDate: string | null, nombreEmpresa: string) : string {
+    const parsedStartDate = new Date(startDate);
+    const parsedEndDate = new Date(endDate ?? new Date());
+
+    let yearDiff = parsedEndDate.getUTCFullYear() - parsedStartDate.getUTCFullYear();
+    let monthDiff = parsedEndDate.getUTCMonth() - parsedStartDate.getUTCMonth();
+
+    if (monthDiff < 0) {
+        yearDiff -=  1;
+        monthDiff += 12;
+    }
+
+    return this.generatedMessage(yearDiff,monthDiff);
+  }
+
+  private generatedMessage(yearDiff: number, monthDiff: number): string
+  {
+    let parts = [];
+
+    if (yearDiff > 0) {
+        parts.push(`${yearDiff} año${yearDiff > 1 ? 's' : ''}`);
+    }
+
+    if (monthDiff > 0) {
+        parts.push(`${monthDiff} mes${monthDiff > 1 ? 'es' : ''}`);
+    }
+
+    return parts.join(" y ") || "0 meses";
+  }
+
   public takeStartDate(date: string): number {
     return new Date(date).getUTCFullYear();
   }
